959a25a7285f94f428bf6b7ab724e900e0e22eb4
ultimatefaloe <ultimatefaloe@outlool.com>
2026-03-26 15:01:47 +0100

    script update

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,5 +3,15 @@
 // For SQLite: DATABASE_URL="file:./dev.db"
 // For PostgreSQL: DATABASE_URL="postgresql://user:password@localhost:5432/logistics"
 
+import 'dotenv/config'
 
-
+export default {
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+    // seed: `tsx prisma/seed.ts`,
+  },
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+};
